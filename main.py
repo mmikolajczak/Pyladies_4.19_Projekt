7f7ea8fd62e81545e8cff42c6b56beb96737ebcd
mmikolajczak <mkm0796@gmail.com>
2018-05-09 19:11:58 +0200

    wczytywanie

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Hello there :)
+DATA_PATH = './szwagropol_data/transactions.txt'
+
+with open(DATA_PATH, encoding='utf-8') as f:
+    rows = []
+    for line in f:
+        current_row = line.strip().split(';')
+        rows.append(current_row)
+
+for i in range(5):
+    print(rows[i])
+
